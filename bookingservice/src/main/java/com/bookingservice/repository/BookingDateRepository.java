8512bc91116756decb216c25df8793134eac002b
nehamm687 <74143408+nehamm687@users.noreply.github.com>
2025-11-05 13:26:10 +0530

    Delete bookingservice directory

--- bookingservice/src/main/java/com/bookingservice/repository/BookingDateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookingservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookingservice.entity.BookingDate;
-
-public interface BookingDateRepository extends JpaRepository<BookingDate, Long> {
-
-}
--- bookingservice/src/main/java/com/bookingservice/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookingservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookingservice.entity.Bookings;
-
-public interface BookingRepository extends JpaRepository<Bookings, Long> {
-
-}
